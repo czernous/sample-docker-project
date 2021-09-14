d336f6376eedcc08f0dc9578f610a28d32816c13
Anton Chernous <a.chernous@notamedia.ru>
2021-09-14 15:33:51 +0300

    update docker launch scripts

--- docker-run-dev.sh
+++ docker-run-dev.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker-compose -f docker-compose.yml -f docker-compose.development.yml up --build -d
+docker-compose -f docker-compose.yml -f docker-compose.development.yml up --build -d 
+echo -e "\033[33mremoving dangling images"
+docker image prune -f 
--- docker-run-prod.sh
+++ docker-run-prod.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 docker-compose up --build -d
+echo -e "\033[33mremoving dangling images"
+docker image prune -f 
